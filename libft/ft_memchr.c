8097920d8ebd23bd87ffcc4f61e33997e9c05142
paranoidm42 <cemlur125@gmail.com>
2023-07-06 12:36:34 +0300

    a

---
+++ libft/ft_memchr.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ccur <ccur@k1m14s08.42kocaeli.com.tr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/06 01:33:22 by ccur              #+#    #+#             */
+/*   Updated: 2023/07/06 01:33:22 by ccur             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t			i;
+	unsigned char	*buffer;
+
+	buffer = (unsigned char *)s;
+	i = 0;
+	while (i < n)
+	{
+		if (*buffer == (unsigned char)c)
+			return (buffer);
+		buffer++;
+		i++;
+	}
+	return (0);
+}
