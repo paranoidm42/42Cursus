5dd744541d945d278f34642c4794bed97f67467b
paranoidm42 <cemlur125@gmail.com>
2023-07-07 02:21:51 +0300

    Update ft_memchr.c

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -1,30 +1,30 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_memchr.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ccur <ccur@k1m14s08.42kocaeli.com.tr>      +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/06 01:33:22 by ccur              #+#    #+#             */
-/*   Updated: 2023/07/06 13:09:15 by ccur             ###   ########.tr       */
-/*                                                                            */
-/* ************************************************************************** */
+	/* ************************************************************************** */
+	/*                                                                            */
+	/*                                                        :::      ::::::::   */
+	/*   ft_memchr.c                                        :+:      :+:    :+:   */
+	/*                                                    +:+ +:+         +:+     */
+	/*   By: ccur <ccur@k1m14s08.42kocaeli.com.tr>      +#+  +:+       +#+        */
+	/*                                                +#+#+#+#+#+   +#+           */
+	/*   Created: 2023/07/06 01:33:22 by ccur              #+#    #+#             */
+	/*   Updated: 2023/07/06 13:09:15 by ccur             ###   ########.tr       */
+	/*                                                                            */
+	/* ************************************************************************** */
 
-#include "libft.h"
+	#include "libft.h"
 
-void	*ft_memchr(const void *s, int c, size_t n)
-{
-	size_t			i;
-	unsigned char	*buffer;
-
-	buffer = (unsigned char *)s;
-	i = 0;
-	while (i < n)
+	void	*ft_memchr(const void *s, int c, size_t n)
 	{
-		if (*buffer == (unsigned char)c)
-			return (buffer);
-		buffer++;
-		i++;
+		size_t			i;
+		unsigned char	*buffer;
+
+		buffer = (unsigned char *)s;
+		i = 0;
+		while (i < n)
+		{
+			if (*buffer == (unsigned char)c)
+				return (buffer);
+			buffer++;
+			i++;
+		}
+		return (0);
 	}
-	return (0);
-}
